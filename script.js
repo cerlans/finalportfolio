e109b5e9c04c407e25cdd2aa5b37a2d6f3d1c963
cerlans <kgomez222@hotmail.com>
2021-09-22 13:23:02 -0400

    3 files modified

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-console.log('hello!')
+let buttonForMenu = document.getElementById('buttonForMenu')
+
+console.log(buttonForMenu)
+
+
+buttonForMenu.addEventListener('click', ()=>{
+  let hiddenMenu = document.getElementById('hiddenMenu');
+  if(hiddenMenu.classList.contains('invisibleMenu')){
+    hiddenMenu.classList.remove('invisibleMenu');
+    hiddenMenu.classList.add('visibleMenu');
+  } else if (hiddenMenu.classList.contains('visibleMenu')){
+    hiddenMenu.classList.remove('visibleMenu')
+    hiddenMenu.classList.add('invisibleMenu');
+  }
+})
